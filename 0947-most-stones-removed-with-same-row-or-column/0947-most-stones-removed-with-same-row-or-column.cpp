8dd880f58b933ecd7475731325bff2c75a815391
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2022-11-17 15:48:42 +0530

    Time: 485 ms (36.12%), Space: 15.4 MB (47.75%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,34 +1,94 @@
 class Solution {
 public:
-    int c;
-    int n ;
+//     int c;
+//     int n ;
    
     
-    void dsf(int index , vector<vector<int>>&a , vector<bool>&visit )
-    {
-        visit[index] = true;
-        for(int i = 0 ; i< n ; i++)
-        {
-            if(visit[i] ==false && (a[index][0] == a[i][0] || a[index][1] == a[i][1]) )
-                dsf(i , a,visit );
-        }
-    }
+//     void dsf(int index , vector<vector<int>>&a , vector<bool>&visit )
+//     {
+//         visit[index] = true;
+//         for(int i = 0 ; i< n ; i++)
+//         {
+//             if(visit[i] ==false && (a[index][0] == a[i][0] || a[index][1] == a[i][1]) )
+//                 dsf(i , a,visit );
+//         }
+//     }
    
-    int removeStones(vector<vector<int>>&a) {
-         n = a.size();
-         c = 0 ;
-         vector<bool>visit(n , false);
+//     int removeStones(vector<vector<int>>&a) {
+//          n = a.size();
+//          c = 0 ;
+//          vector<bool>visit(n , false);
+        
+//         for(int i = 0 ; i<n ; i++)
+//         {
+//             if(visit[i] == false){
+//              c++ ;
+//             dsf(i , a , visit);
+//             }
+            
+//         }
+        
+//        return n-c ;
+//     }
+    
+    
+    //to find the root user
+    
+    struct DisjointSet{
+        vector<int> parent;
+        vector<int> size;
+        
+        DisjointSet(int maxSize){
+            parent.resize(maxSize);
+            size.resize(maxSize);
+            for(int i = 0; i < maxSize; ++i){
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+        
+        int find_set(int x){
+            if(x == parent[x])return x;
+            return parent[x] = find_set(parent[x]);
+        }
+        
+        void union_set(int a, int b){
+            a = find_set(a);
+            b = find_set(b);
+            if(a != b){
+                if(size[a] < size[b])swap(a, b);
+                parent[b] = a;
+                size[a] += size[b];
+            }
+        }
+    };
+    
+    int removeStones(vector<vector<int>>&a){
+        int n = a.size();
+        set<int>s;
+        
+        DisjointSet dsu(n);
         
         for(int i = 0 ; i<n ; i++)
         {
-            if(visit[i] == false){
-             c++ ;
-            dsf(i , a , visit);
+            for(int j = i+1; j < n; ++j){
+                if(a[i][0] == a[j][0] or a[i][1] == a[j][1]){
+                    dsu.union_set(i, j);
+                }
             }
-            
         }
         
-       return n-c ;
+        
+        
+        for(int i = 0 ; i<n;i++)
+        {
+            s.insert(dsu.find_set(i));
+        }
+        
+        
+        
+        
+        return n - s.size();
     }
 };
 
