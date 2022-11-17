8e6cf22ab2319709e257781571793ba34e437c34
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2022-11-17 11:35:09 +0530

    Time: 277 ms (57.90%), Space: 14.1 MB (75.96%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    int cnt;
-    vector<int> v;
-    void dfs(int curr, vector<vector<int>> &a){
-        v[curr] = 1;
-        for(int i = 0; i < a.size(); ++i){
-            if(!v[i] and (a[curr][0] == a[i][0] or a[curr][1] == a[i][1])){
-                dfs(i, a);
-            }
+    int c;
+    int n ;
+   
+    
+    void dsf(int index , vector<vector<int>>&a , vector<bool>&visit )
+    {
+        visit[index] = true;
+        for(int i = 0 ; i< n ; i++)
+        {
+            if(visit[i] ==false && (a[index][0] == a[i][0] || a[index][1] == a[i][1]) )
+                dsf(i , a,visit );
         }
     }
+   
     int removeStones(vector<vector<int>>&a) {
-        int n = a.size();
-        v.assign(n, 0);
-        cnt = 0;
-        for(int i = 0; i < n; ++i){
-            if(!v[i]){
-                ++cnt;
-                dfs(i, a);
+         n = a.size();
+         c = 0 ;
+         vector<bool>visit(n , false);
+        
+        for(int i = 0 ; i<n ; i++)
+        {
+            if(visit[i] == false){
+             c++ ;
+            dsf(i , a , visit);
             }
+            
         }
-        return n - cnt;
+        
+       return n-c ;
     }
 };
+
+
