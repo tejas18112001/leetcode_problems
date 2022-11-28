ae04a72019533abeefd0eb107cd519bb8ca0d082
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2022-11-28 19:20:07 +0530

    Time: 350 ms (54.51%), Space: 74.2 MB (18.02%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,89 +1,40 @@
-
-
-/*
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         
-        int i = 0 , j = 0  , cnt = 0 , n = fruits.size()  , res = INT_MIN ; 
-        unordered_set<int>p;
-        unordered_map<int,int>q ;
+        int i = 0 , j = 0  , cnt = 0 , n = fruits.size()  , res = INT_MIN , q =0;               unordered_set<int>p;
+        vector<int>a(fruits.size() , 0);
         
         while(j<n)
         {
-            q[fruits[j]] ++ ;
+            a[fruits[j]] ++ ;
             if(p.find(fruits[j]) == p.end())
             {
-                
                 p.insert(fruits[j]);
-                if(p.size() > 2)
-                {
-                while(p.size()>2 && j>=i)
+            
+            }
+          
+            
+            while(p.size()>2)
             {
-                    
-             
-                    q[fruits[i]] --;
-                    if(q[fruits[i]] == 0)
-                        q.erase(fruits[i]) ;
-                     i++ ;
-            }
                 
+                while(j>=i && p.size()>2)
+                {
+                    a[fruits[i]] --;
+                    if(a[fruits[i]] == 0)
+                        p.erase(fruits[i]) ;
+                    i++ ;
+                }
+               
+            
+                             
             }
-                
-            }
-           
-            res = max(res , j - i+1);
+         
+            res = max(res , j-i+1);
             j++;
         }
+        if(res == -2147483648)
+            return fruits.size();
         return res ;
-    }
-};
-
-
-
-
-
-*/
-
-
-
-
-
-
-
-
-class Solution {
-public:
-    int totalFruit(vector<int>& tree) {
-       unordered_map<int, int>cnt ;
-        
-        set<int> st;
-        st.clear();
-        
-        int i, j;
-        i = 0; j = 0;
-        
-        int ans = 0;
-        
-        while(j<tree.size()) {
-            cnt[tree[j]] ++;
-            if (st.find(tree[j]) == st.end()) {
-                st.insert(tree[j]);
-                if (st.size() > 2) {
-                    while (st.size() > 2 && j >= i) {
-                        cnt[tree[i]] --;
-                        if (cnt[tree[i]] == 0) {
-                            st.erase(tree[i]);
-                        }
-                        i ++;
-                    }
-                }
-            }
-            
-            ans = max(ans, j-i + 1);
-            j++ ;
-        }
-        
-        return ans;
     }
 };
