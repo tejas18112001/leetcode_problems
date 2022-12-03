e48eb78b439d5eb59cf07e58182f0876c24ed2e1
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2022-12-03 22:33:45 +0530

    Time: 311 ms (5.02%), Space: 7.8 MB (98.31%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,33 +1,52 @@
 class Solution {
 public:
+    
+bool p(char a , char b)
+{
+        //return counts[a]>counts[b] || (counts[a] == counts[b] && a<b) ;
+            return a>b ;
+
+}
     string frequencySort(string s) {
-         int n = s.size() ;
-        unordered_map<char , int>p ;
-        vector<string>bucket(n+1 , "");
-        string res ;
+//          int n = s.size() ;
+//         unordered_map<char , int>p ;
+//         vector<string>bucket(n+1 , "");
+//         string res ;
        
-        for(int i = 0 ; i < n ; i++)
+//         for(int i = 0 ; i < n ; i++)
+//         {
+//             p[s[i]] ++;
+//         }
+        
+//         for(auto i:p)
+//         {    int frq ; char chr ;
+//              frq = i.second ;
+//              chr = i.first ;
+//            // bucket[frq] += chr ;
+//              bucket[frq].append(frq , chr) ;
+//         }
+        
+        
+//         for(int i = n ; i>0 ; i--)
+//         {
+//             if(bucket[i] != "" )
+//             {
+//                 res += bucket[i] ;
+//             }
+//         }
+//         return res ;
+        
+        unordered_map<char , int>counts;
+        for (char ch : s)
+            ++counts[ch];
+        sort(s.begin(), s.end() , [&](char a , char b)
         {
-            p[s[i]] ++;
-        }
+                 return counts[a] > counts[b] || (counts[a] == counts[b] && a>b )  ;
+        }) ;  
         
-        for(auto i:p)
-        {    int frq ; char chr ;
-             frq = i.second ;
-             chr = i.first ;
-           // bucket[frq] += chr ;
-             bucket[frq].append(frq , chr) ;
-        }
+      return s;
         
         
-        for(int i = n ; i>0 ; i--)
-        {
-            if(bucket[i] != "" )
-            {
-                res += bucket[i] ;
-            }
-        }
-        return res ;
         
     }
-};
+}; 
