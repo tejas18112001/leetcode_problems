7408c464cda921a7aaae124a4c83650cacfb1d48
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2022-12-12 18:04:09 +0530

    Time: 2 ms (43.13%), Space: 6.3 MB (15.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,27 +7,22 @@ public:
             return 1 ;
         if(n==2)
             return 2 ;
-        if(v[n-1] != 0)
-            return v[n-1] ;
+        if(v[n] != 0)
+            return v[n] ;
         
-        v[n-1] = result(n-1 , v) + result(n-2 , v) ;
-        return v[n-1] ;
-        }
+        v[n] = result(n-1 , v) + result(n-2 , v) ;
+        return v[n] ;
+    }
+    
+    
+    
     
     int climbStairs(int n) {
         
        
         
        vector<int>v(n+1 , 0) ; 
-//         v[0] = 0 ;
-//         v[1] = 1 ;
-//         v[2] = 2 ;
-//         int res = 0 ;
-//         for(int i = 3 ; i<=n ;i++)
-//         {
-//            v[i] = v[i-1] + v[i-2] ; 
-//         }
-        
+
        
         return result(n , v) ;
     }
