8f02afd957f41f9dcc8395f47c31220f2696d5d6
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2022-12-15 04:31:03 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (78.38%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size() ;
-        vector<int>res(n ,0);
+       // vector<int>res(n ,0);
         int rob1 = 0 , rob2 = 0 ;
        
-        if(n ==1)
-            return nums[0] ;
+//         if(n ==1)
+//             return nums[0] ;
         
-        res[0] = nums[0] ;
-        res[1] = max(nums[0] , nums[1]) ;
-        if(n == 2)
-            return res[1] ;
-        for(int i = 2 ; i<n ; i++) 
+//         res[0] = nums[0] ;
+//         res[1] = max(nums[0] , nums[1]) ;
+        // if(n == 2)
+        //     return res[1] ;
+        for(int i = 0 ; i<n ; i++) 
         {
             
-            res[i] = max(nums[i] +res[i-2] , res[i-1] ) ;
+            int t = max(nums[i] +rob1 , rob2 ) ;
+            rob1 = rob2 ;
+            rob2 = t ;
         }
         
-            return res[n-1] ;;
+           return rob2 ;
     }
 };
