195789fd1505c4b08238d23d5b938396a3de9b7e
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2022-12-19 16:21:00 +0530

    Time: 460 ms (34.33%), Space: 103 MB (49.43%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,51 +2,25 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& p) {
         int n = p.size() ;
-        vector<int>ans(n ,0);     
-        map<int , int>mp ;
-   
-
-        
-        // for(int i = 0 ; i <n ; i++){
-        //     mp[i] = 0 ; 
-        //     cout<<mp[i]<<" ";
-        // }
-        
-        stack<int>st;
-        
-          
-        for(int i = 0 ; i<n ; i++){
-            
-            if(st.empty())
-            {
-                st.push(i) ;
-                continue ;
-            }
-            
-           while(!st.empty() && p[st.top()] < p[i] ){
-               
-              
-               mp[st.top()] = i  - st.top() ;
-                
-               //ans[i] = st.top() - i ;
-               st.pop();
-           }
-            
-            
-             st.push(i) ;
-             
-        }
-        
-        while(!st.empty()) {
-         
-        st.pop() ;
-        }
-        
+        vector<int>res(n ,0);     
+         stack<int>st;
        
         for(int i = 0 ; i<n ; i++){
-            p[i] = mp[i] ;
+           // if(st.empty()){
+           //      st.push(i) ;
+           //      continue ;
+           // }
+            
+           while(!st.empty() && p[st.top()] < p[i] ){
+                res[st.top()] = i  - st.top() ;
+                st.pop();
+           }
+            
+            st.push(i) ;
+             
         }
-         return p  ;
+    
+         return res ;
       
     }
         
