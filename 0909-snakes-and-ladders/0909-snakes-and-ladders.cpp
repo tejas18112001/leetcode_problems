6f67c11a2d27a1d51b3a76e71b30913213974a41
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-01-24 17:44:32 +0530

    Time: 179 ms (7.29%), Space: 21 MB (10.94%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int t ;
+    
+     vector<int>inttopos(int Int , vector<vector<int>>& board) {
+         reverse(board.begin() , board.end()) ;
+         int r ,c ;
+         r = (Int-1) / t ;
+         c = (Int-1) % t ;
+         if(r %2) {
+             c = (t-1-c);
+         }
+         return {r ,c} ;
+     }
+    
+    int solution(vector<vector<int>>& board) {
+        vector<bool>p(t*t+2 , false); 
+        queue<vector<int>>q ;
+        
+        q.push({1,0});
+        p[1] = true;    
+          
+        while(!q.empty()) {
+            int currpos = q.front()[0] ;
+            int move = q.front()[1] ;
+            q.pop();
+            
+            for(int i =1; i<=6; i++) {
+                int nextpos = currpos + i ;
+                int r ,c ;
+                 
+                r = inttopos(nextpos , board)[0];
+                c = inttopos(nextpos , board)[1] ;
+                if(board[r][c] != -1) {
+                    nextpos = board[r][c] ;
+                }
+                
+                if(nextpos == t*t) {
+                    return move+1 ;
+                }
+                
+                if(p[nextpos] == false) {
+                    p[nextpos] = true ;
+                    q.push({nextpos , move+1}) ;
+                    
+                    
+                }
+                
+                
+            }
+            
+        }
+        return -1 ;
+    }
+    
+    
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int n = board.size() ;
+        t = n ;
+        if(n*n<=6) return 1;
+        cout<<inttopos(31,board)[1];
+        
+        int r , c ;
+        r = c = n;
+        return solution(board);
+        
+         
+    }
+};
