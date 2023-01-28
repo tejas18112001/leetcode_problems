397b033b94e3d788741bf3cb72528fa4c1261d9c
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-01-28 08:28:13 +0530

    Time: 10 ms (14.92%), Space: 8 MB (32.97%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>>ans ;
     
-    void solution(vector<int>& nums  ,vector<int>temp  ,vector<bool>p, int n , int ind) {
-        if(temp.size() == n){
-            ans.push_back(temp) ;
+     vector<vector<int>>ans;
+    
+
+    void solution(vector<int>&nums , int n , int ind){
+       
+       if(ind == n){
+            ans.push_back(nums) ;
             return ;
         }
         
-        for(int i = 0 ; i< n ; i++) {
-            if( !p[i] ) {
-            temp .push_back(nums[i]) ;
-            p[i] = true ;
-            solution(nums , temp , p , n , i+1) ;
-            p[i] = false ;
-            temp.pop_back() ;
-            }
+        for(int i = ind ; i<n ; i++){
+          swap(nums[ind] , nums[i]) ;
+          solution(nums ,n ,ind+1) ;
+          swap(nums[ind] , nums[i]) ;
+          
         }
     }
+    
     vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size() ;
+        int n =  nums.size() ;
         vector<int>temp ;
-        vector<bool>visited(n ,0) ;
-        solution(nums ,temp ,visited, n , 0) ;
-        return ans ;
+        temp = nums ;
+        solution(nums  , n , 0) ;
+        return ans;
     }
 };
