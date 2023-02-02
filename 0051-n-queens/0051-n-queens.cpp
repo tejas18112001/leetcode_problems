704e37010e2974c9b05651e384710fcd11f4de43
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-02-02 17:35:28 +0530

    Time: 22 ms (28.50%), Space: 16.1 MB (19.88%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,53 +1,67 @@
 class Solution {
 public:
-    
+    /* ⏲\U0001f4a1 complexity analysis ⬇ 
+    isSafe function take 3.O(n) time complexity
+      
+    isSafe called inside solution function()
+    so overall time complexity is O(n!) 3*O(n)*O(n) ~= O(N2) ;
+    */
     vector<vector<string>>ans ;
-    bool isSafe(int row , int col , int n , vector<string>&temp) {
-        int r ,c ;
-        r = row , c = col ;
-        // checking north west ↖   
-        // row ⬇ and col ⬇
-        while(row >= 0 && col >= 0 ){
+//     bool isSafe(int row , int col , int n , vector<string>&temp) {
+//         int r ,c ;
+//         r = row , c = col ;
+//         // checking north west ↖   
+//         // row ⬇ and col ⬇
+//         while(row >= 0 && col >= 0 ){
             
-            if(temp[row][col] == 'Q' )
-                return false ;
-            row -- ,col-- ;
-        }
+//             if(temp[row][col] == 'Q' )
+//                 return false ;
+//             row -- ,col-- ;
+//         }
         
-        //restore origional position of row and col ;
-        row = r  , col = c ;
+//         //restore origional position of row and col ;
+//         row = r  , col = c ;
         
-        // checking west south ↙
-        // row ⬆ col ⬇
-        while(row < n && col>=0){
-            if(temp[row][col] == 'Q')
-                return false ;
-            row ++ , col-- ;
-        }
+//         // checking west south ↙
+//         // row ⬆ col ⬇
+//         while(row < n && col>=0){
+//             if(temp[row][col] == 'Q')
+//                 return false ;
+//             row ++ , col-- ;
+//         }
         
         
-        //restore origional position of row and col ;
-        row = r  , col = c ;
-        //checking ⬅ direction
-        // row constant col ⬇
-        while(col>= 0) {
-             if(temp[row][col] == 'Q')
-                return false ;
-              col-- ;
-        }
-        return true ;
-    }
-    void solution(int col , vector<string>temp , int  n) {
+//         //restore origional position of row and col ;
+//         row = r  , col = c ;
+//         //checking ⬅ direction
+//         // row constant col ⬇
+//         while(col>= 0) {
+//              if(temp[row][col] == 'Q')
+//                 return false ;
+//               col-- ;
+//         }
+//         return true ;
+//     }
+    void solution(
+         int col , vector<string>temp , int  n , vector<int>rowLeft ,                              vector<int>upperDiagonal ,    vector<int>lowerDiagonal
+    ) {
         
         for(int row = 0 ; row<n ;row++) {
             if(col == n){
                 ans.push_back(temp) ;
                 return ;
             }
-            if(isSafe(row , col , n , temp))
+            if(rowLeft[row] == 0 && upperDiagonal[row+col] == 0 &&  lowerDiagonal[n-1 + (row - col )] == 0 )
             {
                 temp[row][col] = 'Q' ;
-                solution(col+1 , temp ,n) ;
+               
+                rowLeft[row] = 1 ;
+                upperDiagonal[row+col] = 1 ;
+                lowerDiagonal[n-1 + (row - col )] = 1 ;
+                 solution(col+1 , temp ,n , rowLeft ,upperDiagonal , lowerDiagonal) ;
+                 rowLeft[row] = 0 ;
+                upperDiagonal[row+col] = 0 ;
+                lowerDiagonal[n-1 + (row - col )] = 0 ;
                 temp[row][col] = '.' ;
                 
             }
@@ -59,7 +73,10 @@ public:
         string s(n , '.');
         for(int i = 0 ; i<n ;i++)
             temp[i] = s ;
-        solution(0 , temp ,n) ;
+   vector<int>rowLeft(n ,0) ;
+   vector<int>upperDiagonal(n+n ,0) ; 
+   vector<int>lowerDiagonal(n+n , 0) ;
+        solution(0 , temp ,n ,rowLeft ,upperDiagonal , lowerDiagonal) ;
         return ans ;
     }
 };
