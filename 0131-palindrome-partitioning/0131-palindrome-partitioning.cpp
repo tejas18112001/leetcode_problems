2f7a09fe2da4871bc8fc27f5b9b0f36a458987c2
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-02-28 16:41:11 +0530

    Time: 239 ms (26.89%), Space: 171.3 MB (7.87%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    vector<vector<string>>ans ;
+    
+    bool checkPal(int i , int j , string s) {
+        while(i < j) {
+            if(s[i] != s[j]) {
+                return false ;
+            }
+            i++ , j -- ;
+        }
+      return true ;
+    }
+    
+    void solution(string s , vector<string>temp , int i) {
+        
+        if(i >= s.size()) {
+            ans.push_back(temp) ;
+            return ;
+        }
+        for(int k  = i ; k<s.size() ; k++) {
+            if(checkPal(i , k , s)) {
+                temp.push_back(s.substr(i , k-i+1)) ;
+                solution(s ,temp , k+1 ) ;
+                temp.pop_back() ;
+            }
+        }
+        
+         return ;
+    }
+    
+    
+    vector<vector<string>> partition(string s) {
+        vector<string>temp ;
+        solution(s , temp , 0) ;
+        return ans ;
+    }
+};
