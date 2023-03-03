7cb9c9055dbc0b84d7a34e34582c96bb461e0dfc
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-03-03 22:53:03 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (69.99%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     int strStr(string h, string ne) {
         
-        int n = h.size() , n1 = ne.size() ;
-        if(n < n1) return -1 ;
-        int ans ;
-        int i = 0 ;
-        int d = 0 ; 
         
-        while(i < n && (n-i >= n1 )) {
+         return h.find(ne);
+//         int n = h.size() , n1 = ne.size() ;
+//         if(n < n1) return -1 ;
+//         int ans ;
+//         int i = 0 ;
+//         int d = 0 ; 
+        
+//         while(i < n && (n-i >= n1 )) {
             
             
-            int ind = 0;
+//             int ind = 0;
 
-              while(i<n && ind < n1 && h[i] == ne[ind]) {
-              if(ind == n1-1 && h[i] == ne[ind])
-                  return d ;
-                  ind ++ , i++ ;
-              }
+//               while(i<n && ind < n1 && h[i] == ne[ind]) {
+//               if(ind == n1-1 && h[i] == ne[ind])
+//                   return d ;
+//                   ind ++ , i++ ;
+//               }
            
-            d++;
-            i = d ;
-        }
-        return -1;
+//             d++;
+//             i = d ;
+//         }
+//         return -1;
     }
 };
