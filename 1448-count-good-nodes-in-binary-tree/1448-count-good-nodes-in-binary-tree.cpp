0af3fab8b77898e279122367d4be606140161951
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-03-29 22:37:13 +0530

    Time: 152 ms (31.33%), Space: 86.4 MB (61.29%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -13,26 +13,17 @@ class Solution {
 public:
     int ans = 0 ;
     
-    void checkGoodN(string str , int val) {
-        int flag = 0 ;
-        for(int i = 0 ; i<str.size() ; i++) {
-            if( (int (str[i]) - '0') > val){
-                flag = 1 ;
-                break ;
-            }
-        }
-        if(flag == 0) ans++;
-    }
-    
     void solution(TreeNode*root , int mx) {
         if(root == NULL) {
             return ;
         }
         
        
-        if(root->val >= mx)
-            ans++ ;
-        mx = max(mx , root->val) ;
+        if(root->val >= mx) {
+           ans++ ;
+           mx = root->val;
+        }
+            
        
         solution(root->left , mx) ;
        
@@ -40,9 +31,7 @@ public:
     }
     
     int goodNodes(TreeNode* root) {
-        if(!root) return 0 ;
-        if(!root->left && !root->right) return 1 ;
-        string s = "" + to_string(root->val) ;
+       
         int mx = root->val ;
         solution(root , mx) ;
         return ans ;
