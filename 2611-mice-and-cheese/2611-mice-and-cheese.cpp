ccb97d9c3111539ef0481be4f5c9fc9efc1b8f4b
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-04-02 10:30:48 +0530

    Time: 138 ms (90.91%), Space: 101.6 MB (36.36%) - LeetHub

--- 2611-mice-and-cheese/2611-mice-and-cheese.cpp
+++ 2611-mice-and-cheese/2611-mice-and-cheese.cpp
@@ -6,17 +6,22 @@ public:
     int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
         priority_queue<int>pq ;
         int ans = 0 ;
-        ans =  accumulate(reward2.begin() , reward2.end() ,0) ;\
-         
+        
         for(int i = 0 ; i<reward1.size() ; i++) {
-            pq.push(reward1[i] - reward2[i]) ;
+            // pq.push(reward1[i] - reward2[i]) ;
+            reward1[i] -= reward2[i] ;
+            ans += reward2[i] ;
         }
         
-        while(k > 0) {
-            ans += pq.top() ;
-            pq.pop() ;
+        sort(reward1.begin() , reward1.end()) ;
+      
+        while(k >0 ) {
+            ans += reward1.back() ;
+            reward1.pop_back() ;
             k-- ;
+          
         }
+      
         
         return ans ;
     }
