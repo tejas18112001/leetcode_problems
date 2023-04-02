dadb4d599e31cc4d406815118bce13ba5b4c45dd
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-04-02 18:13:39 +0530

    Time: 3 ms (78.12%), Space: 12.2 MB (70.97%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -22,7 +22,7 @@ public:
             return !root1 && !root2;
         }
         
-       // if( root1->val != root2->val) return false ;
+       if( root1->val != root2->val) return false ;
         
 //        bool left1 = flipEquiv(root1->left , root2->left) ;
 //        bool right1 ;
@@ -40,7 +40,7 @@ public:
        
        
            
-      return root1->val == root2->val && ((flipEquiv(root1->left , root2->left ) && 
+      return ((flipEquiv(root1->left , root2->left ) && 
                flipEquiv(root1->right , root2->right) )  || 
            (flipEquiv(root1->left , root2->right) &&
             flipEquiv(root1->right , root2->left) ) );
