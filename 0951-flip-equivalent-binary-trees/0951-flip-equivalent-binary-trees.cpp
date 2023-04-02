f32c8517c5ba0913f01de206fdc0c615e00d18c9
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-04-02 18:53:18 +0530

    Time: 0 ms (100.00%), Space: 13.2 MB (7.43%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -14,61 +14,49 @@ public:
     
    
     
-    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        // (either left1 == left2 && right1 == right2 ) || 
-        // (left1 == right2  && right1 == left2)
+    void solution(TreeNode*root , vector<int>&a) {
+        if(!root) return  ; 
+        a.push_back(root->val) ;
+        int l = root->left != NULL ? root->left->val : -1 ;
         
-        if(!root1 || !root2 ) {
-            return !root1 && !root2;
+        int r = root->right != NULL ? root->right->val : -1 ;
+        if(l < r) {
+            solution(root->left , a) ;
+            solution(root->right , a) ;
+        }else {
+            solution(root->right , a) ;
+            solution(root->left , a) ;
         }
         
-       if( root1->val != root2->val) return false ;
+        a.push_back(-100) ;
         
-//        bool left1 = flipEquiv(root1->left , root2->left) ;
-//        bool right1 ;
-       
-//              right1 = flipEquiv(root1->right , root2->right) ;
-//         }else 
-//             right1 = false ;
-        
-//         bool left2 = flipEquiv(root1->left , root2->right) ;
-        
-//        bool right2  ;
-//       if(left2) right2 = flipEquiv(root1->right , root2->left) ;
-//         else 
-//             right2 = false ;
-       
-       
-           
-      return ((flipEquiv(root1->left , root2->left ) && 
-               flipEquiv(root1->right , root2->right) )  || 
-           (flipEquiv(root1->left , root2->right) &&
-            flipEquiv(root1->right , root2->left) ) );
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+//         // (either left1 == left2 && right1 == right2 ) || 
+//         // (left1 == right2  && right1 == left2)
+        
+//         if(!root1 || !root2 ) {
+//             return !root1 && !root2;
+//         }
+        
+//        if( root1->val != root2->val) return false ;
+        
+//        return ((flipEquiv(root1->left , root2->left ) && 
+//                flipEquiv(root1->right , root2->right) )  || 
+//            (flipEquiv(root1->left , root2->right) &&
+//             flipEquiv(root1->right , root2->left) ) );
+        
+        vector<int>a , b ;
+        solution(root1 , a) ;
+        solution(root2 , b) ;
+        return a == b ;
+        
+        
+        
+        
+    }
+     
     
     
 };
