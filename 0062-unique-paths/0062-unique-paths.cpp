0231cfd33cb3757a74fa64c46b6f4d28636e3a01
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-04-06 16:59:20 +0530

    Time: 2 ms (45.26%), Space: 6.6 MB (33.70%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,30 +3,34 @@ public:
     
     int solution(int m , int n  ,  vector<vector<int>>&dp) {
          if(m == 1 || n == 1) {
-           dp[m][n] = 1 ;
+          
            return 1 ;
        }
         
-      if(dp[m][n] != 0) {
+      if(dp[m][n] != 0 ) {
           return dp[m][n] ; 
       }
     
-        else {
+    
+     else {
             
       int path1 = solution(m-1 , n , dp) ;
       int path2 = solution(m , n-1 , dp) ;
-      dp[m][n] = path1 + path2 ;
+      dp[m][n] =  path1 + path2 ;
+      if(n <=  min(n ,m) && m <= min(n , m) )
+        dp[n][m] = path1 + path2 ;
+      
             
     }
      
     return dp[m][n] ;
         
-    }
+}
     
     int uniquePaths(int m, int n) {
        vector<vector<int>>dp(m + 1, vector<int>(n +1 ,0)) ;
         int ans = solution(m , n , dp) ;
         return ans ;
-            // return 
+            
     }
 };
