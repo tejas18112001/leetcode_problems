892ffb71feafd6dea849b83db735d29a2f5a4f93
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-04-15 00:46:34 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.50%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-       stack<char>st ;
-       int l = 0 , r = 0 ; 
-       for(char i : s) {
-           if(i == '(' || i == '{'|| i == '[') {
-               st.push(i) ;
-              
-           }
-           
-           else {
-               
-           if(st.empty()) return false ;
-           else if (i == ')' && st.top() == '(') 
-                st.pop() ;
-           else if (i == '}' && st.top() == '{')
-               st.pop() ;
-           else if(i == ']' && st.top() == '[')
-               st.pop() ;
-           else return false ;
-           }
-       }
-        if(!st.empty()) return false ;
-        return true ;
+       
+        stack<char>st ;
+        for(auto i : s) {
+            if(i == '(' || i == '[' || i == '{') 
+                st.push(i) ;
+            else {
+                // if(st.empty()) return false ;
+                if(i == ')' && !st.empty() && st.top() == '(') st.pop() ;
+                else if(i == ']' && !st.empty()  && st.top() == '[') st.pop() ;
+                else if(i == '}' && !st.empty() && st.top() == '{')st.pop() ;
+                else 
+                    return false ;
+            }
+        }
+        
+        return st.empty()?true : false ;
+        
     }
 };
