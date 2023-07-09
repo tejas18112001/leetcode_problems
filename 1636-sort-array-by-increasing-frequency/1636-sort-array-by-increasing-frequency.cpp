181580dc9a0b8a6a4a00c09b0dbb6ca19d706374
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-07-09 07:33:47 +0530

    Time: 20 ms (5.37%), Space: 11.2 MB (80.68%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int , int>mp;
+        for(auto i : nums) {
+             mp[i]++ ;
+        }
+        
+        sort(nums.begin() , nums.end() , [&](int a , int b){
+            if(mp[a] == mp[b]) {
+                return a > b ;
+            }
+            return mp[a]<mp[b] ;
+        }) ;
+        return nums ;
+    }
+};
