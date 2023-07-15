0521a18942c2157011a4c84663c02e9ce579d62c
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-07-15 16:52:48 +0530

    Time: 35 ms (33.88%), Space: 19.3 MB (5.88%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,70 +10,61 @@
  */
 class Solution {
 public:
-
     
     
-  
-    ListNode*merge_two_lists(ListNode*&a , ListNode*&b) {
-        ListNode*root = new ListNode(0) ;
-        ListNode*curr = root ;
+ ListNode* mergeTwolist(ListNode*list1 , ListNode*list2) {
+ ListNode*head = new ListNode() , *ans = head;
         
-        while(a or b) {
-            if(!a) {
-               curr->next = b ;
+        while(list1 != NULL && list2 != NULL) {
+           ListNode*node = new ListNode() ;
+            if(list1->val < list2->val) {
                
-            
-               break ;
+                node->val = list1->val ;
+                list1 = list1->next ;
+                
+            }else {
+                node->val = list2->val ;
+                list2 = list2->next ;
             }
             
-             else if(!b) {
-                curr->next = a ; 
-              
-                break ;
-            }
-            
-            else if(a->val <= b->val) {
-                
-                curr->next = a ;
-                curr = curr->next ;
-                a = a->next ;
-                
-                
-            }
-            
-             else{
-                
-                curr->next = b ;
-                 curr = curr->next ;
-                b = b->next ;
-                
-            }
+            head->next = node ;
+            head = head->next ;
             
             
-        }
-         
-         return root->next ;
+            
+            
+            
     }
-   
+        if(list1 == NULL) {
+            head->next = list2 ;
+        }else
+            head->next = list1 ;
+        
+        
+        
+        return ans->next ;
+        
+}
     
     
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         int n = lists.size() ;
-        if(n == 0) return NULL ;
-        while(n > 1) {
-            vector<ListNode*>a ;
-            for(int i = 0 ; i<n; i+=2) {
-                if(i == n-1) a.push_back(lists[i]) ;
-                else {
-                    cout<<1<<"\n" ;
-                    a.push_back(merge_two_lists(lists[i] , lists[i+1])) ;
+        if(n == 0  ) return NULL ;
+        
+        while(n>1) {
+            vector<ListNode*>p ;
+            for(int i = 0 ; i<n ; i+=2) {
+                if(i == n-1){
+                    p.push_back(lists[i]) ;
+                }else {
+                    p.push_back(mergeTwolist(lists[i] , lists[i+1])) ;
                 }
             }
             
-            lists = a ;
+            lists = p ;
             n = lists.size() ;
         }
+        
         return lists[0] ;
     }
-    
 };
