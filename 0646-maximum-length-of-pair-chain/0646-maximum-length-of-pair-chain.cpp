40b87f4b1a9b974ec29a41d565de057fcaff46f0
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-08-27 10:30:31 +0530

    Time: 180 ms (63.61%), Space: 22.8 MB (55.02%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,33 +1,55 @@
 class Solution {
 public:
     
-    static bool cmp(vector<int>&a , vector<int>&b) {
+//     static bool cmp(vector<int>&a , vector<int>&b) {
        
         
-        return a[1]<b[1] ;
+//         return a[1]<b[1] ;
         
-    }
+//     }
     
+//     int findLongestChain(vector<vector<int>>& pairs) {
+//          int n = pairs.size() ;
+//          sort(pairs.begin() , pairs.end() , cmp) ;
+        
+//          for(auto i: pairs) {
+//              for(auto j : i) {
+//                  cout<<j<<" " ;
+//              }
+//              cout<<endl ;
+//          }
+//          int ans = 1 ; 
+//          pair<int ,int>a = {pairs[0][0] , pairs[0][1]} ;
+        
+//         for(int i = 1  ; i<n ; i++) {
+//             if(pairs[i][0] > a.second) {
+//                 a.first = pairs[i][0] ;
+//                 a.second = pairs[i][1] ;
+//                 ans++ ;
+//             }
+//         }
+//         return ans ;
+//     }
+    
+    
+   
     int findLongestChain(vector<vector<int>>& pairs) {
-         int n = pairs.size() ;
-         sort(pairs.begin() , pairs.end() , cmp) ;
+        int n = pairs.size() ;
+        sort(pairs.begin(),pairs.end());
         
-         for(auto i: pairs) {
-             for(auto j : i) {
-                 cout<<j<<" " ;
-             }
-             cout<<endl ;
-         }
-         int ans = 1 ; 
-         pair<int ,int>a = {pairs[0][0] , pairs[0][1]} ;
-        
-        for(int i = 1  ; i<n ; i++) {
-            if(pairs[i][0] > a.second) {
-                a.first = pairs[i][0] ;
-                a.second = pairs[i][1] ;
-                ans++ ;
+        vector<int>dp(n , 1) ;
+        for(int i = 1 ; i<n ; i++) {
+            for(int j = 0 ; j<i ; j++) {
+                if(pairs[j][1] < pairs[i][0]) {
+                    dp[i] = max(dp[i] , dp[j]+1) ;
+                }
             }
         }
+        int ans = -1001 ;
+        for(int i = 0 ; i<n ; i++) {
+            ans = max(ans , dp[i]) ;
+        }
+        
         return ans ;
     }
 }; 
