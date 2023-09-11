39ddcada91db0fc1f242d5d5d6ce953d3846a091
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2023-09-11 15:38:22 +0530

    Time: 92 ms (10.62%), Space: 77.3 MB (5.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -4,21 +4,23 @@ public:
     vector<vector<int>>ans ;
     
     void solution(vector<int>&nums , int target , int index , vector<int>temp) {
+       
         if(index >= nums.size()) {
-        
+            
+            if(target == 0) {
+                ans.push_back(temp) ;
+            }    
+            
         return  ;
     }
     
-    if(target == 0){
-         ans.push_back(temp) ;
-         return  ;
-    }
+   if(target < 0)return ;
     
-    if(target >= nums[index]) {
+    // if(target >= nums[index]) {
         temp.push_back(nums[index]) ;
         solution(nums , target - nums[index]  ,index , temp) ;
         temp.pop_back() ;
-    }
+    // }
     
     solution(nums , target  , index + 1 , temp) ;
    
