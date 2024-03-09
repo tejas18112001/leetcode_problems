5e81683abe2976276317cf6892c3c4da58c0e014
tejas18112001 <89287728+tejas18112001@users.noreply.github.com>
2024-03-09 13:56:48 +0530

    Time: 3 ms (37.84%), Space: 8.1 MB (70.52%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,63 +1,57 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int>ans;
-        int l ,r,t,b,d ;
-        b = matrix.size()-1;
-        t = 0;
-        l = matrix[0].size()-1;
-        r = 0;
-        // d = 0 (l ->right ),1 (top ->down ),2(right to left ),3 (down to top
-        d = 0;
-       int total = matrix.size() * matrix[0].size() ;
-       int sum = 0 ;
+        int l , r , t, b ;
+        r= 0 , l = matrix[0].size() ;
+        t = 0 , b = matrix.size() ;
         
-      while(total > sum) 
-      { 
-         
-        if(d == 0)
-        {
-            for(int i = r ; i<=l ; i++)
-            {
-                ans.push_back(matrix[t][i]) ;
-                sum++;
+        vector<int>ans ;
+        int d= 0 ;
+        
+        int sum = 0 ;
+        int length = l*b ;
+        l-- , b-- ;
+        
+        while(sum < length) {
+            
+            if(d == 0) {
+                for(int i =  r ; i<= l ; i++) {
+                    sum++ ;
+                    ans.push_back(matrix[t][i]) ;
+                }
+                t += 1 ;
+            }
+            
+            else if(d == 1) {
+                for(int i = t ; i <= b ; i++) {
+                    sum++ ;
+                    ans.push_back(matrix[i][l]) ;
+                }
+                l = l-1 ;
+            }
+            
+            else if(d == 2) {
+                for(int i = l ; i>= r ; i--) {
+                    sum++ ;
+                    ans.push_back(matrix[b][i]) ;
+                }
+                b = b-1 ;
+            }
+            
+            else {
+                
+                for(int i = b ; i >= t ; i-- ) {
+                    sum ++ ;
+                    ans.push_back(matrix[i][r]) ;
+                    
+                }
+                r += 1 ;
+                
                 
             }
-            t+=1; 
+            d = (d+1)%4 ;
         }
         
-        else if(d == 1) // in top to bottom
-        {
-            for(int i = t ; i<=b ; i++)
-            {
-                ans.push_back(matrix[i][l]);
-                 sum++;  
-            }
-             l -= 1 ;
-        }
-        
-        else if(d ==2) // right to left 
-        {
-            for(int i = l ; i>=r ; i--)
-            {
-                ans.push_back(matrix[b][i]);
-                sum++;
-            }
-            b -= 1;
-        }
-        
-        else
-        {
-            for(int i =b ;i>=t ;i--)
-            {
-                ans.push_back(matrix[i][r]);
-                sum++;
-            }
-            r += 1;
-        }
-        
-        d = (d+1)%4 ;
-    }
         return ans ;
     }
 };
